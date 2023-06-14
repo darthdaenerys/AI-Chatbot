76613c5bf4b9f85c62ea2c0e6b40fee9f2dd6fa3
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-06-14 15:07:01 +0530

    Add clean text

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 import os
 
 def load_data():
@@ -9,4 +10,30 @@ def load_data():
     df=pd.concat([df,df2],axis=0)
     del df2
     return df
-    
+
+def clean_text(text):
+    text=re.sub('-',' ',text.lower())
+    text=re.sub('[.]',' . ',text)
+    text=re.sub('[1]',' 1 ',text)
+    text=re.sub('[2]',' 2 ',text)
+    text=re.sub('[3]',' 3 ',text)
+    text=re.sub('[4]',' 4 ',text)
+    text=re.sub('[5]',' 5 ',text)
+    text=re.sub('[6]',' 6 ',text)
+    text=re.sub('[7]',' 7 ',text)
+    text=re.sub('[8]',' 8 ',text)
+    text=re.sub('[9]',' 9 ',text)
+    text=re.sub('[0]',' 0 ',text)
+    text=re.sub('[,]',' , ',text)
+    text=re.sub('[?]',' ? ',text)
+    text=re.sub('[!]',' ! ',text)
+    text=re.sub('[$]',' $ ',text)
+    text=re.sub('[&]',' & ',text)
+    text=re.sub('[/]',' / ',text)
+    text=re.sub('[:]',' : ',text)
+    text=re.sub('[;]',' ; ',text)
+    text=re.sub('[*]',' * ',text)
+    text=re.sub('[\']',' \' ',text)
+    text=re.sub('[\"]',' \" ',text)
+    text=re.sub('\t',' ',text)
+    return text
