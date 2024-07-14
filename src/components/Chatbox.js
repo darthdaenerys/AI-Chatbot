6c1de85f09eea4a5768c62b0924b5d2bd7a127ae
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-07-14 11:03:40 +0530

    Add function createNewChatElement

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -5,5 +5,13 @@ import { useEffect, useState } from 'react';
 
 export default function Chatbox() {
 	const [input, setInput] = useState("");
+
+	const createNewChatElement=()=>{
+		let rawhtml = `<div class="message-wrapper"><div class="message left"><p><span id="cursor">|</span></p></div></div>`;
+		let chatsElement=document.querySelector('.chats');
+		chatsElement.insertAdjacentHTML('beforeend',rawhtml);
+	}
+	
+
 	
 }
