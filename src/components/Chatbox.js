67f79e238c5d8d7c4a6c340ceed562aeefc3b2fe
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-07-21 13:04:48 +0530

    Add try catch statement

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -81,7 +81,20 @@ export default function Chatbox() {
 		const button=document.querySelector('.btn');
 		if(button.disabled) return;
 		button.disabled=true;
-		
+		try {
+			if(!input.trim()) return;
+			addUserChatElement(input);
+			const response = await fetch('http://localhost:5000/message', {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ "message": input })
+			});
+			setInput("");
+		} catch (error) {
+			console.log('There was an error: ', error);
+		}
 	}
 	
 }
