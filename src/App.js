22796fb1c48ebd1551e0f58b9b70961caa0c62d7
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-07-24 14:53:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Chatbox from './components/Chatbox';
+import Navbar from './components/Navbar';
+
+
+function App() {
+	return (
+		<>
+			<Navbar title="AI Chat Application" />
+		</>
+	);
+}
+
+export default App;
