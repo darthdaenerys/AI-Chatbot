7c684181aa877320c0bbc82231d5519bcf39012d
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-07-30 16:24:10 +0530

    Add load_data method

--- utilities/process.py
+++ utilities/process.py
@@ -16,3 +16,12 @@ vocab_size=data['vocab_size']
 max_sequence_length=data['max_sequence_length']
 buffer_size=data['buffer_size']
 batch_size=data['batch_size']
+
+def load_data():
+    df=pd.read_csv('../data/dialogue.txt',sep='\t',names=['question','answer'],encoding='utf-8')
+    df2=pd.read_csv(os.path.join('../data','chatbot dataset.txt'),names=['question','answer'],sep='\t',encoding='utf-8')
+    df=pd.concat([df,df2],axis=0)
+    df2=pd.read_csv(os.path.join('../data','dialogs_expanded.csv'),usecols=['question','answer'],encoding='utf-8',sep='\t')
+    df=pd.concat([df,df2],axis=0)
+    del df2
+    return df
