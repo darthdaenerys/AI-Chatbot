50e2971e8a8daa81421466b6d2f3399ba838b7da
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-08-01 12:02:19 +0530

    Add preprocess method

--- utilities/process.py
+++ utilities/process.py
@@ -52,3 +52,19 @@ def clean_text(text):
     text=re.sub('[\"]',' \" ',text)
     text=re.sub('\t',' ',text)
     return text
+
+def preprocess(df):
+    df['question tokens']=df['question'].apply(lambda x:len(x.split()))
+    df['answer tokens']=df['answer'].apply(lambda x:len(x.split()))
+    show_qna_tokens(df)
+    df.drop(columns=['answer tokens','question tokens'],axis=1,inplace=True)
+    df['encoder_inputs']=df['question'].apply(clean_text)
+    df['decoder_targets']=df['answer'].apply(clean_text)+' <eos>'
+    df['decoder_inputs']='<sos> '+df['answer'].apply(clean_text)+' <eos>'
+    df['encoder input tokens']=df['encoder_inputs'].apply(lambda x:len(x.split()))
+    df['decoder input tokens']=df['decoder_inputs'].apply(lambda x:len(x.split()))
+    df['decoder target tokens']=df['decoder_targets'].apply(lambda x:len(x.split()))
+    show_encdec_tokens(df)
+    df.drop(columns=['question','answer','encoder input tokens','decoder input tokens','decoder target tokens'],axis=1,inplace=True)
+
+    
