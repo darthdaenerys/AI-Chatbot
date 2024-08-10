62f4b17e0047ca2a03589ab8bf8ef151253bd976
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-08-10 16:46:48 +0530

    Create encoder decoder model

--- chat.py
+++ chat.py
@@ -9,3 +9,9 @@ with open('../hyperparameters.json','r') as f:
 lstm_cells=data['lstm_cells']
 embedding_dim=data['embedding_dim']
 vocab_size=data['vocab_size']
+
+encoder=Encoder(lstm_cells,embedding_dim,vocab_size)
+decoder=Decoder(lstm_cells,embedding_dim,vocab_size)
+chatbot=ChatBot(encoder,decoder)
+chatbot.load_weights('../weights/weight')
+text=''
