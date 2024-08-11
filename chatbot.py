ab855f11a4618194533a973388f091bb72e97513
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-08-11 13:26:07 +0530

    Add init method

--- chatbot.py
+++ chatbot.py
@@ -4,3 +4,11 @@ from utilities.utils import *
 from utilities.process import *
 from utilities.vocab import get_vocabulary
 import time
+
+class ChatBot(tf.keras.models.Model):
+    def __init__(self,base_encoder,base_decoder,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.vectorize_layer=get_vocabulary()
+        self.encoder,self.decoder=self.build_inference_model(base_encoder,base_decoder)
+        self.first_word=True
+        self.eos_recieved=False
