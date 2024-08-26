c71bd26b64d878bd715269fd555dfc6d0008b82f
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-08-26 13:36:30 +0530

    Create encode class

--- models.py
+++ models.py
@@ -1,2 +1,16 @@
 import tensorflow as tf
 from tensorflow.keras.layers import LSTM,Dense,Embedding,Dropout,LayerNormalization
+
+class Encoder(tf.keras.models.Model):
+    def __init__(self,units,embedding_dim,vocab_size,*args,**kwargs) -> None:
+        super().__init__(*args,**kwargs)
+        self.units=units
+        self.vocab_size=vocab_size
+        self.embedding_dim=embedding_dim
+        self.embedding=Embedding(
+            vocab_size,
+            embedding_dim,
+            name='encoder_embedding',
+            mask_zero=True,
+            embeddings_initializer=tf.keras.initializers.GlorotNormal()
+        )
