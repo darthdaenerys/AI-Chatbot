49d4edad7d2ab2ed2dc90ceb1507f7535b430178
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-09-06 18:49:10 +0530

    Add loss and correct methods

--- trainer.py
+++ trainer.py
@@ -5,3 +5,18 @@ class ChatBotTrainer(tf.keras.models.Model):
         super().__init__(*args,**kwargs)
         self.encoder=encoder
         self.decoder=decoder
+
+    def loss_fn(self,y_true,y_pred):
+        loss=self.loss(y_true,y_pred)
+        mask=tf.math.logical_not(tf.math.equal(y_true,0))
+        mask=tf.cast(mask,dtype=loss.dtype)
+        loss*=mask
+        return tf.reduce_mean(loss)
+            
+    def correct_fn(self,y_true,y_pred):
+        pred_values = tf.cast(tf.argmax(y_pred, axis=-1), dtype='int64')
+        correct = tf.cast(tf.equal(y_true, pred_values), dtype='float64')
+        mask = tf.cast(tf.greater(y_true, 0),dtype='float64')
+        n_correct = tf.keras.backend.sum(mask*correct)
+        n_total = tf.keras.backend.sum(mask)
+        return n_correct,n_total
