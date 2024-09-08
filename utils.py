a8e5d334d876f2b1c5eefeedae8880ff03a75ae9
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-09-08 21:34:14 +0530

    Update utils

--- utils.py
+++ utils.py
@@ -1,2 +1,10 @@
 def sequences2ids(sequence,vectorize_layer):
     return vectorize_layer(sequence)
+
+def ids2sequences(ids,vectorize_layer):
+    decode=''
+    if type(ids)==int:
+        ids=[ids]
+    for id in ids:
+        decode+=vectorize_layer.get_vocabulary()[id]+' '
+    return decode
