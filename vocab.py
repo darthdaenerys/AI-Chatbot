7661957702be01579614a1fa7f899d06bda4fbfe
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-09-09 11:02:58 +0530

    Add vectorize layer

--- vocab.py
+++ vocab.py
@@ -14,3 +14,12 @@ def get_vocabulary():
     df['decoder input tokens']=df['decoder_inputs'].apply(lambda x:len(x.split()))
     df['decoder target tokens']=df['decoder_targets'].apply(lambda x:len(x.split()))
     df.drop(columns=['question','answer','encoder input tokens','decoder input tokens','decoder target tokens'],axis=1,inplace=True)
+
+    vectorize_layer=TextVectorization(
+        max_tokens=vocab_size,
+        standardize=None,
+        output_mode='int',
+        output_sequence_length=max_sequence_length
+    )
+    vectorize_layer.adapt(df['encoder_inputs']+' '+df['decoder_targets']+' <sos> <eos>',batch_size=512)
+    return vectorize_layer
